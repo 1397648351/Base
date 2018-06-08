82d281b7469f93f67588140315d4673c007f5eb8
1397648351 <wuzedc@163.com>
2018-06-08 14:56:05 +0800

    1

---
+++ DBUtility/MysqlHelper.cs
@@ -0,0 +1,26 @@
+﻿/************************************************************************ 
+ * 项目名称 ：DBUtility
+ * 项目描述 ：
+ * 文件名称 ：MysqlHelper.cs
+ * 版 本 号 ：v1.0.0.0
+ * 说    明 ：
+ * 作    者 ：MyPC
+ * IDE 环境 ：Visual Studio 2013
+ * 创建时间 ：2018/6/8 11:08:28
+ * 更新时间 ：2018/6/8 11:08:28
+************************************************************************
+ * Copyright @ Njbosa 2018. All rights reserved.
+************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBUtility
+{
+    public class MysqlHelper
+    {
+    }
+}
---
+++ Test/Actives.cs
@@ -0,0 +1,26 @@
+﻿/************************************************************************ 
+ * 项目名称 ：Test
+ * 项目描述 ：
+ * 文件名称 ：Actives.cs
+ * 版 本 号 ：v1.0.0.0
+ * 说    明 ：
+ * 作    者 ：MyPC
+ * IDE 环境 ：Visual Studio 2013
+ * 创建时间 ：2018/6/8 12:49:33
+ * 更新时间 ：2018/6/8 12:49:33
+************************************************************************
+ * Copyright @ Njbosa 2018. All rights reserved.
+************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class Actives
+    {
+    }
+}
